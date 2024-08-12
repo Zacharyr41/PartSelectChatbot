76b19b35340e3af5ac7c449511296bf76274d14b
Zachary Rothstein <zacharyrothstein@Zacharys-MacBook-Pro.local>
2024-08-11 19:37:46 -0500

    more logging

--- src/query_openai.py
+++ src/query_openai.py
@@ -3,6 +3,7 @@ from query_type import QueryType
 
 import os
 import constants as cts
+import sys
 
 
 def qt_to_filepath(qt: QueryType) -> str:
@@ -34,10 +35,12 @@ async def run_query(query_text: str, qt: QueryType, message_history=[]) -> str:
 
     client = OpenAI()
     message_list = [{"role": "user", "content": prompt}]
+    print("MESSAGE HIST: ", message_history)
     if len(message_history) > 0:
         messages_except_last = message_history[:-1]
         message_list = messages_except_last + message_list
         print("The Message List: ", message_list)
+        sys.stdout.flush()
     completion = client.chat.completions.create(
         model=cts.GPT_MODEL, messages=message_list
     )
